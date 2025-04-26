0ef1a84e73ada64b96875fc581aaeb7ae2cd0a6b
Dawid Bogocz <72217470+dawidbogocz@users.noreply.github.com>
2025-04-26 15:41:53 +0200

    Add project files.

---
+++ NaturalCandles/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NaturalCandles.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
